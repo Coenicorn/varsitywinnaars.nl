29cd2759e1cb83767ea98156990595935e6151e5
Coenicorn <coenicorn>
2024-11-07 10:47:14 +0100

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const firaMono = localFont({
+  src: "./fonts/FiraMono.woff2",
+  variable: "--font-fira-mono",
+  weight: "100 900"
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={firaMono.className}>
         {children}
       </body>
     </html>
